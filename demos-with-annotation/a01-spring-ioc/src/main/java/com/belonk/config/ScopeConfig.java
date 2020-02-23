b599abdb413ab1e37389602435b8f96cb2438fd4
sun <belonk.sun@abcbooking.cn>
2020-02-23 15:18:50 +0800

    add scope/lazy load test code.

---
+++ demos-with-annotation/a01-spring-ioc/src/main/java/com/belonk/config/ScopeConfig.java
@@ -0,0 +1,103 @@
+package com.belonk.config;
+
+import com.belonk.bean.LazyBean;
+import com.belonk.bean.PrototypeBean;
+import com.belonk.bean.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * Created by sun on 2020/2/22.
+ *
+ * @author sunfuchang03@126.com
+ * @since 1.0
+ */
+@Configuration
+public class ScopeConfig {
+	/*
+	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 *
+	 * Static fields/constants/initializer
+	 *
+	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 */
+
+
+
+	/*
+	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 *
+	 * Instance fields
+	 *
+	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 */
+
+
+
+	/*
+	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 *
+	 * Constructors
+	 *
+	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 */
+
+
+
+	/*
+	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 *
+	 * Methods
+	 *
+	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	 */
+
+	/**
+	 * 默认情况下，Spring管理的bean都是单例的。在IOC容器完成创建之前，会创建所有bean。
+	 *
+	 * @return
+	 */
+	@Bean
+	public User user() {
+		System.out.println("创建User对象...");
+		User user = new User();
+		user.setName("spring");
+		return user;
+	}
+
+	/**
+	 * 默认情况下，Spring管理的bean都是单例的。在IOC容器完成创建之前，会创建所有bean。
+	 * <p>
+	 * 可以通过@Scope注解来标识Spring 管理的bean的范围，包括以下值：
+	 * - singleton: 单例，只有一个bean实例，默认值
+	 * - prototype: 原型，每次获取bean斗殴创建一个新实例
+	 * - request: 每次请求创建一个bean实例
+	 * - session: 每个session一个bean实例
+	 * 其中，request和session需要在web环境中。
+	 * <p>
+	 * 如果scope为prototype，则容器创建时并不会创建bean，而是在获取bean时创建bean。
+	 *
+	 * @return
+	 */
+	@Bean
+	@Scope("prototype")
+	public PrototypeBean prototypeBean() {
+		System.out.println("创建PrototypeBean对象...");
+		return new PrototypeBean();
+	}
+
+	/**
+	 * 默认情况下，单例bean会在容器创建时创建，可以使用@Lazy注解来使bean被延迟加载，被延迟加载的bean并不会在容器创建
+	 * 时创建，而是在第一次获取bean时创建。
+	 *
+	 * @return
+	 */
+	@Bean
+	@Lazy
+	public LazyBean lazyBean() {
+		System.out.println("创建LazyBean...");
+		return new LazyBean();
+	}
+}
--- demos-with-annotation/a01-spring-ioc/src/test/java/com/belonk/test/AnnotationIOCTest.java
+++ demos-with-annotation/a01-spring-ioc/src/test/java/com/belonk/test/AnnotationIOCTest.java
@@ -1,6 +1,9 @@
 package com.belonk.test;
 
+import com.belonk.bean.LazyBean;
+import com.belonk.bean.PrototypeBean;
 import com.belonk.bean.User;
+import com.belonk.config.ScopeConfig;
 import com.belonk.config.SpringConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -51,7 +54,7 @@ public class AnnotationIOCTest {
 	 */
 
 	@Test
-	public void test1() {
+	public void testScanBean() {
 		// 构造基于注解配置的IOC容器，传入配置类
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
 		User                               user               = applicationContext.getBean(User.class);
@@ -62,7 +65,7 @@ public class AnnotationIOCTest {
 	}
 
 	@Test
-	public void test2() {
+	public void testScanBean2() {
 		// 构造基于注解配置的IOC容器，传入配置类
 		AnnotationConfigApplicationContext applicationContext  = new AnnotationConfigApplicationContext(SpringConfig.class);
 		int                                beanDefinitionCount = applicationContext.getBeanDefinitionCount();
@@ -84,5 +87,48 @@ public class AnnotationIOCTest {
 		userService
 		user
 		 */
+	}
+
+	@Test
+	public void testScopeDefault() {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ScopeConfig.class);
+		System.out.println("IOC容器创建完成...");
+		User user  = applicationContext.getBean(User.class);
+		User aUser = applicationContext.getBean(User.class);
+		System.out.println(user == aUser);
+		/*~:
+		创建User对象...
+		IOC容器创建完成...
+		true
+		*/
+	}
+
+	@Test
+	public void testScopePrototype() {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ScopeConfig.class);
+		System.out.println("IOC容器创建完成...");
+		PrototypeBean prototypeBean1 = applicationContext.getBean(PrototypeBean.class);
+		PrototypeBean prototypeBean2 = applicationContext.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1 == prototypeBean2);
+		/*~:
+		IOC容器创建完成...
+		创建PrototypeBean对象...
+		创建PrototypeBean对象...
+		false
+		*/
+	}
+
+	@Test
+	public void testLazy() {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ScopeConfig.class);
+		System.out.println("IOC容器创建完成...");
+		LazyBean lazyBean1 = applicationContext.getBean(LazyBean.class);
+		LazyBean lazyBean2 = applicationContext.getBean(LazyBean.class);
+		System.out.println(lazyBean1 == lazyBean2);
+		/*~:
+		IOC容器创建完成...
+		创建LazyBean...
+		true
+		*/
 	}
 }
