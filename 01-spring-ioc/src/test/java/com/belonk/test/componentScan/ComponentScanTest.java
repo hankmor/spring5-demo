14fe5f665760de9f97a2bf95ee2fc033ce4d99a5
sun <sunfuchang03@126.com>
2020-03-12 23:44:07 +0800

    添加@ComponentScan注解测试代码

--- 01-spring-ioc/src/test/java/com/belonk/test/componentScan/ComponentScanTest.java
+++ 01-spring-ioc/src/test/java/com/belonk/test/componentScan/ComponentScanTest.java
@@ -3,7 +3,9 @@ package com.belonk.test.componentScan;
 import com.belonk.bean.User;
 import com.belonk.config.BasicComponentScanConfig;
 import com.belonk.config.ComponentScanFilterConfig;
+import com.belonk.config.ComponentScanLazyConfig;
 import com.belonk.config.CustomFilterConfig;
+import com.belonk.controller.UserController;
 import com.belonk.test.BaseIOCTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,60 +17,60 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @since 1.0
  */
 public class ComponentScanTest extends BaseIOCTest {
-	/*
-	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	 *
-	 * Static fields/constants/initializer
-	 *
-	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	 */
+    /*
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     *
+     * Static fields/constants/initializer
+     *
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
 
 
 
-	/*
-	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	 *
-	 * Instance fields
-	 *
-	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	 */
+    /*
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     *
+     * Instance fields
+     *
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
 
 
 
-	/*
-	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	 *
-	 * Constructors
-	 *
-	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	 */
+    /*
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     *
+     * Constructors
+     *
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
 
 
 
-	/*
-	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	 *
-	 * Methods
-	 *
-	 * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	 */
+    /*
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     *
+     * Methods
+     *
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
 
-	@Test
-	public void testBasicComponentScan() {
-		// 构造基于注解配置的IOC容器，传入配置类
-		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicComponentScanConfig.class);
-		User                               user               = applicationContext.getBean(User.class);
-		System.out.println("user : " + user);
+    @Test
+    public void testBasicComponentScan() {
+        // 构造基于注解配置的IOC容器，传入配置类
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BasicComponentScanConfig.class);
+        User user = applicationContext.getBean(User.class);
+        System.out.println("user : " + user);
 		/*~:
 		user : User(name=spring)
 		*/
-	}
+    }
 
-	@Test
-	public void testComponentScanFilter() {
-		// 被@Controller标注且未实现MyFilter接口的类会被扫描
-		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ComponentScanFilterConfig.class);
-		printBeans(applicationContext);
+    @Test
+    public void testComponentScanFilter() {
+        // 被@Controller标注且未实现MyFilter接口的类会被扫描
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ComponentScanFilterConfig.class);
+        printBeans(applicationContext);
 		/*~:
 		bean count: 7
 		org.springframework.context.annotation.internalConfigurationAnnotationProcessor
@@ -79,13 +81,13 @@ public class ComponentScanTest extends BaseIOCTest {
 		componentScanFilterConfig
 		myUserController
 		 */
-	}
+    }
 
-	@Test
-	public void testCustomFilterComponentScan() {
-		// 包含user或者User的bean都会被扫描
-		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(CustomFilterConfig.class);
-		printBeans(applicationContext);
+    @Test
+    public void testCustomFilterComponentScan() {
+        // 包含user或者User的bean都会被扫描
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(CustomFilterConfig.class);
+        printBeans(applicationContext);
 		/*~:
 		bean count: 11
 		org.springframework.context.annotation.internalConfigurationAnnotationProcessor
@@ -100,5 +102,19 @@ public class ComponentScanTest extends BaseIOCTest {
 		userDao
 		userService
 		 */
-	}
+    }
+
+    @Test
+    public void testComponentScanWithLazyBean() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ComponentScanLazyConfig.class);
+        System.out.println("容器创建完成...");
+        // 由于@ComponentScan配置了lazyInit=true，所以在获取bean时在创建bean，而不是容器创建完成就已经创建
+        UserController userController = applicationContext.getBean(UserController.class);
+        System.out.println("获取UserController完成...");
+		/*~:
+		容器创建完成...
+        创建UserController...
+        获取UserController完成...
+		 */
+    }
 }
