f1273d4158256bfbb01309ccc02581774111de06
sun <belonk.sun@abcbooking.cn>
2020-04-29 15:39:41 +0800

    add test code

--- 01-spring-ioc/src/main/java/com/belonk/imports/config/ImportConfig.java
+++ 01-spring-ioc/src/main/java/com/belonk/imports/config/ImportConfig.java
@@ -4,6 +4,7 @@ import com.belonk.imports.bean.Cat;
 import com.belonk.imports.bean.Dog;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by sun on 2020/3/16.
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.Import;
 // @Import可以导入单个类，也可以导入实现了ImportSelector接口或ImportBeanDefinitionRegistrar的自定义导入选择器
 // 导入的类，bean名称是类的全限定名，如com.belonk.bean.Cat
 @Import(value = {Cat.class, Dog.class, AnimalImportSelector.class, AnimalImportBeanDefinitionRegistrar.class})
+@ImportResource(locations = {"classpath:beans.xml"})
 public class ImportConfig {
     /*
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
